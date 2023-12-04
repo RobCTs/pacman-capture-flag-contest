6c8d55ea943a591f0b3b296822af2e95af319b27
Nazanin <nazaninia@gmail.com>
2023-12-04 03:06:56 +0100

    better action pick, todo: numcarrying!!!

--- pacman-agent-main/pacman-agent/myTeam.py
+++ pacman-agent-main/pacman-agent/myTeam.py
@@ -139,30 +139,91 @@ class GoodCaptureAgent(CaptureAgent):
 
 class OffensiveGoodAgent(GoodCaptureAgent):
     """
-  An agent that seeks food. This is an agent
+  A reflex agent that seeks food. This is an agent
   we give you to get an idea of what an offensive agent might look like,
   but it is by no means the best or only way to build an offensive agent.
   """
-    global numCarrying
-    numCarrying = 0
+    global numCarrying 
+    numCarrying = 0 
+    
+    def getNumCarrying(self, game_state, action):
+        currState = self.get_previous_observation() #current observation food list same as successor, gotta use previous  
+        successor = self.get_successor(game_state, action)
+        food_list = self.get_food(successor).as_list()
+        if currState != None:
+            curr_food_list = self.get_food(currState).as_list()
+            #print("currFoodLength: ", len(curr_food_list))     
+            #print("succFoodLength: ", len(food_list))
+            diff = len(curr_food_list) - len(food_list)
+            if 'numCarrying' in locals():
+                numCarrying += diff 
+                print("NUMCARRYING: ", numCarrying)
+            if diff < 0: #negative difference means more food in future state that current state, i.e. pacman has died
+                #reset nr of carrying food to 0
+                numCarrying = 0 #num_succ
+        else:
+            #no previous observation available e.g. first/starting state
+            numCarrying = 0
+        if 'numCarrying' in locals() or 'numCarrying' in globals():
+            return numCarrying
+        return 0
+    
+    def choose_action(self, game_state):
+        actions = game_state.get_legal_actions(self.index)
+        actions.remove(Directions.STOP) #again useless to get stuck
+        values = []
+        for a in actions:
+            numCarrying = self.getNumCarrying(game_state, a)
+            print("CHOOSE ACTION NUMCARRYING: ", numCarrying)
+            v = self.MCTS(game_state, a, 5, 0, numCarrying)
+            values.append(v)
+        #print("Values1: ", values)
+        #values = [self.evaluate(game_state, a, numCarrying) for a in actions]
+        #print("Values2: ", values)
+        maxValue = max(values)
+        bestActions = [a for a, v in zip(actions, values) if v == maxValue]
 
-    #TODO: rewrite the ReflexCaptureAgent's get action method
-    def get_action(self, game_state):
-        return super().get_action(game_state)
-
-    def get_features(self, game_state, action):
+        return bestActions[0]
+    
+    #to update global value from within a function, one has to pass it along every time
+    def evaluate(self, game_state, action, numCarrying):
+        print("evaluate NUMCARRYING: ", numCarrying)
+        features = self.get_features(game_state, action, numCarrying)
+        weights = self.get_weights(game_state, action)
+        return features * weights
+    
+    #TODO: possibly improve using ucb (https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/)
+    def MCTS(self, game_state, action, depth, num_iters, numCarrying):
+        print("MCTS NUMCARRYING: ", numCarrying)
+        n = self.get_successor(game_state, action)
+        actions = n.get_legal_actions(self.index)
+        #print("actions: ", actions)
+        actions.remove(Directions.STOP) #not doing anything is never a good choice
+        if depth > 0:
+            # random simulation 
+            a = random.choice(actions)
+            value = self.evaluate(game_state, action, numCarrying)
+            # backpropagate and update parent nodes based on value of newly added curr child
+            value = value + 0.2 ** num_iters * self.MCTS(n, a, depth - 1, num_iters + 1, numCarrying)
+            return value
+        else:
+            return self.evaluate(game_state, action, numCarrying)
+    
+    def get_features(self, game_state, action, numCarrying):
         features = util.Counter()
         successor = self.get_successor(game_state, action)
+        
+        my_pos = successor.get_agent_state(self.index).get_position()
+        
         food_list = self.get_food(successor).as_list()
         features['successor_score'] = -len(food_list)  # self.getScore(successor)
 
         # Compute distance to the nearest food
         if len(food_list) > 0:  # This should always be True,  but better safe than sorry
-            my_pos = successor.get_agent_state(self.index).get_position()
             min_distance = min([self.get_maze_distance(my_pos, food) for food in food_list])
+            #print("MINDISTANCE: ", min_distance)
             features['distance_to_food'] = min_distance
 
-        
         cap_list = self.get_capsules(successor)
         # Compute distance to the nearest power pellet/capsule
         if len(cap_list) > 0:
@@ -174,27 +235,42 @@ class OffensiveGoodAgent(GoodCaptureAgent):
         opponents_pos = []
         if len(opp_index) > 0:
             for i in opp_index:
-                tmp = successor.get_agent_position(i)
-                if tmp != None: #get_agent_pos might return None
-                    opponents_pos.append(tmp)
+                if not game_state.data.agent_states[i].is_pacman:
+                    if game_state.data.agent_states[i].scared_timer < 5:
+                        tmp = successor.get_agent_position(i)
+                        if tmp != None: #get_agent_pos might return None
+                            opponents_pos.append(tmp)
+        #only if eligible opponents nearby (ghost and not scared for more than 5 more moves)
         if len(opponents_pos) > 0:    
             min_dist = min([self.get_maze_distance(my_pos, o) for o in opponents_pos])
-            features['distance_to_opponent'] = min_dist#*10
+            features['distance_to_opponent'] = min_dist
+            
+            #special death case (very time consuming so should be avoided most aka have highest weight)
+            #if min_dist <=1:
+            #    features['death'] = 1 #death = 1 if true
+        else:
+            features['distance_to_opponent'] = 0
 
-        # Consider where the home is (e.g. to return after collecting food)
-        #print("gamestate.data.layout.width", game_state.data.layout.width) #returns width of the grid aka e.g. 32 
+         
         # Compute distance to nearest home tile/cell
         valid_homes = []
-        # 2 walls on each side so width of actual home territory = (game_state.data.layout.width-2) /2
-        h_width = (game_state.data.layout.width-2) /2
-        h_height = (game_state.data.layout.height-2) /2
-        for x in range(1,int(h_width+1)):     #TODO possibly check if correct x and y values (e.g. if not start from 0 instead and end at height+1)
-            for y in range(1,int(h_height+1)):
-                if not successor.has_wall(x,y):
-                    valid_homes.append((x,y))
+        # Consider where the home border is (e.g. to return after collecting food)
+        # 2 walls on each side so width of actual home territory is layout-2
+        w = game_state.data.layout.width
+        mid_width = (w-2)/2
+        if self.red == False: #blue territory starts to the right
+            mid_width +=1
+        h_height = game_state.data.layout.height
+        
+        for y in range(1, h_height):
+            if not successor.has_wall(int(mid_width),y):
+                valid_homes.append((int(mid_width),y))
         
         min_home = min([self.get_maze_distance(my_pos, h) for h in valid_homes])
         features['home'] = min_home
+        #if 'min_dist' in locals(): #check if min_dist exists
+        #    if min_dist < 1: #go home if enemy gets too close
+        #        features['home'] = min_home*2
 
         # Consider the amount of food pacman is carrying
         currState = self.get_previous_observation() #current observation food list same as successor, gotta use previous  
@@ -203,21 +279,35 @@ class OffensiveGoodAgent(GoodCaptureAgent):
             print("currFoodLength: ", len(curr_food_list))     
             print("succFoodLength: ", len(food_list))
             diff = len(curr_food_list) - len(food_list)
-
-            numCarrying = diff
-            if diff < 0: #negative difference means more food in future state that current state, e.g. pacman has died
+            
+            numCarrying += diff 
+            
+            if diff < 0: #negative difference means more food in future state that current state, i.e. pacman has died
                 #reset nr of carrying food to 0
                 numCarrying = 0 #num_succ
-            #TODO maybe add something to check if pacman has returned home with the food or not (or does food disappear from food_list once you bring it back home?)
-            print("num Carrying: ", numCarrying)
-            features['numCarrying'] = diff
         else:
             #no previous observation available e.g. first/starting state
-            features['numCarrying'] = 0
+            numCarrying = 0
+        if numCarrying <=2:
+            features['num_carrying'] = numCarrying
+        else: 
+            features['num_carrying'] = numCarrying/3 #force agent to go back more quickly when holding more food
+        print("NUMCARRYING: ", numCarrying)
         return features
 
     def get_weights(self, game_state, action):
-        return {'successor_score': 100, 'distance_to_food': -1}
+        
+        #really force agent to return home consistently after carrying more than 3 food
+        successor = self.get_successor(game_state, action)
+        my_pos = successor.get_agent_state(self.index).get_position()
+        food_list = self.get_food(successor).as_list()
+        
+        if numCarrying >=3:
+            return  {'successor_score': 5, 'distance_to_food': 0, 'distance_to_capsule': 0, 'distance_to_opponent': 1, 'home': -10,
+                'num_carrying': 0}
+        
+        return {'successor_score': 10, 'distance_to_food': -2, 'distance_to_capsule': -1, 'distance_to_opponent': 1, 'home': -1,
+                'num_carrying': 5}
 
 
 class DefensiveGoodAgent(GoodCaptureAgent):
